c4b4a15b0c6878dadc18a8935c00421ba281aad5
Prasanth Venkatesan <onlyyprasanth@gmail.com>
2017-08-13 15:02:39 +0530

    Planet card design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from "./app.routing";
 import { HomeComponent } from './views/home/home.component';
+import { PlanetCardComponent } from './components/planet-card/planet-card.component';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PlanetCardComponent,
+    PlanetsComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
